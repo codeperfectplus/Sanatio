5b6fab08ba2d0cc28d4f5ed2a843cdec1f559cba
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-14 00:13:11 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,36 @@
+import string
+
+
+class Validator(object):
+    
+    def __init__(self):
+        pass
+
+    def equals(self, string1, string2, ignoreCase=False):
+        """ Check if the two string are equal or not """
+        if string1 == string2:
+            return True
+        
+        return False
+
+    def isAlphanumeric(self, string1):
+        if string1.isalnum():
+            return True
+        
+        return False
+
+    def isAlpha(self, string1):
+        if string1.isalpha():
+            return True
+
+        return False
+    
+    def isAscii(self, string1):
+        pass
+
+    
+
+
+
+
 
