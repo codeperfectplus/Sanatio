6d06988d756aef5659534a1000c09f89c5f8b31e
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-15 11:52:58 +0530

    intial commits
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
 import re
 import json
-import sys
-
-sys.path.append('.')
-import string
 from datetime import datetime
+
 from src.utils import all_country
 
 class Validator(object):
@@ -40,7 +37,7 @@ class Validator(object):
             return True
 
         return False
-
+    
     def isPostalCode(self, string1, locale):
         """ check if the string is postal code or not """
         country_data = all_country[locale]
@@ -65,10 +62,49 @@ class Validator(object):
             return True
 
         return False
+    
+    def isPassportNumber(self, string1, locale):  # TODO: research more about passport number
+        """ check if the string is passport number or not """
+        country_data = all_country[locale]
+        
+        PassportNumberRegex = country_data['PassportNumberRegex']
+        if re.match(PassportNumberRegex, string1):
+            return True
+        return False
 
     def isMobilePhone(self, string1, locale):
         """ check if the string is mobile phone or not """
-        pass
+        country_data = all_country[locale]
+
+        MobileNumberRegex = country_data['MobileNumberRegex']
+
+        if re.match(MobileNumberRegex, string1):
+            return True
+
+        return False
+    
+    def isDriverLicense(self, string1, locale):
+        """ check if the string is driver license or not """
+        country_data = all_country[locale]
+
+        DrivingLicenseNumberRegex = country_data['DrivingLicenseNumberRegex']
+
+        if re.match(DrivingLicenseNumberRegex, string1):
+            return True
+
+        return False
+    
+    def isDiscordUsername(self, string1):
+        regex = "^.{3,32}#[0-9]{4}$"
+        if re.match(regex, string1):
+            return True
+        return False
+    
+    def isCreditCard(self, string1):
+        regex = "(^4[0-9]{12}(?:[0-9]{3})?$)|(^(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}$)|(3[47][0-9]{13})|(^3(?:0[0-5]|[68][0-9])[0-9]{11}$)|(^6(?:011|5[0-9]{2})[0-9]{12}$)|(^(?:2131|1800|35\d{3})\d{11}$)"
+        if re.match(regex, string1):
+            return True
+        return False
 
     def equals(self, string1, string2, ignoreCase=False)-> bool:
         """ Check if the two string are equal or not """
@@ -163,7 +199,7 @@ class Validator(object):
 
         return False
 
-    def isDate(self, date1):
+    def isDate(self, date1):  # TODO: add more date format, currently only support YYYY-MM-DD
         """ check if the string is date or not """
         try:
             datetime.strptime(date1, '%Y-%m-%d')
@@ -190,8 +226,9 @@ class Validator(object):
 
     def isDivisibleBy(self, number, divisor):
         """ check if the number is divisible by divisor or not """
-        if number % divisor == 0:
-            return True
+        if self.__isvalidNumber(number) and self.__isvalidNumber(divisor) and divisor != 0:
+            if number % divisor == 0:
+                return True
 
         return False
 
@@ -209,13 +246,26 @@ class Validator(object):
             return True
         return False
 
-    def isIP(self, string1):
+    def isIPV4(self, string1):
         """ check if the string is IP or not """
         regex = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
         if re.search(regex, string1):
             return True
         return False
 
+    def isIPV6(self, string1):
+        regex = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
+        if re.match(regex, string1):
+            return True
+        return False
+
+    def isSSN(self, string1):
+        """ check if the string is SSN or not """
+        regex = '^(?!0{3})(?!6{3})[0-8]\d{2}-(?!0{2})\d{2}-(?!0{4})\d{4}$'
+        if re.match(regex, string1):
+            return True
+        return False
+    
     def isJSON(self, string1):
         """ check if the string is JSON or not """
         try:
@@ -230,22 +280,23 @@ class Validator(object):
 
     def isLatLong(self, string1):
         """ check if the string is lat long or not """
-        pass
+        regex = '^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$'
+        if re.match(regex, string1):
+            return True
+        return False
 
     def isMACAddress(self, string1):
         """ check if the string is MAC address or not """
-        pass
+        regex = '(([0-9a-fA-F]{2}[:]){5}([0-9a-fA-F]{2})|([0-9a-fA-F]{2}[-]){5}([0-9a-fA-F]{2})|[0-9a-fA-F]{12})'
+        if self.__isvalidString(string1) and re.search(regex, string1, re.IGNORECASE):
+            return True
+        return False
 
     def isMD5(self, string1):
         """ check if the string is MD5 or not """
         pass
 
-    def isPassportNumber(self, string1):  # TODO: research more about passport number
-        """ check if the string is passport number or not """
-        if len(string1) == 9 and string1.isalnum():
-            return True
-        return False
-
+   
     def isPort(self, string1):
         """ check if the string is port or not
         A port number is a 16-bit unsigned integer,
@@ -359,7 +410,3 @@ class Validator(object):
         """ remove white space from string """
         if self.__isvalidString(string1):
             return string1.replace(" ", "")
-
-    
-
-
