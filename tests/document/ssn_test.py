dee44c89d6bdb57d784e962b9c9bee801698ab0a
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-17 18:54:40 +0530

    test bug fixed

--- tests/document/ssn_test.py
+++ tests/document/ssn_test.py
@@ -7,4 +7,15 @@ validator = Validator()
 
 
 class SSNTest(unittest.TestCase):
-    pass
+    def test_ssn_true(self):
+        self.assertTrue(validator.isSSN('123-45-6789'))
+        self.assertTrue(validator.isSSN('333-22-4444'))
+        
+    def test_ssn_false(self):
+        self.assertFalse(validator.isSSN('aaa-bbb-cccc'))
+        self.assertFalse(validator.isSSN('900-58-4564'))
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
