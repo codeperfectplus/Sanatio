3eb71e474eb5fe99164637c4c6bda103bf4e3cf1
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-11-19 14:17:11 +0530

    typecheck enabled
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>

--- sanatio/main.py
+++ sanatio/main.py
@@ -12,7 +12,7 @@ class Validator(object):
     def __init__(self):
         pass
 
-    def __isvalidString(self, value):
+    def __isvalidString(self, value: str) -> bool:
         """ check if the string is valid or not """
         if value is None or value == '':
             return False
@@ -22,7 +22,7 @@ class Validator(object):
 
         return False
 
-    def __isvalidNumber(self, value):
+    def __isvalidNumber(self, value: int)-> bool:
         """ check if the number is valid or not """
         if value is None:
             return False
@@ -32,48 +32,49 @@ class Validator(object):
 
         return False
 
-    def __isvalidBoolean(self, value):
+    def __isvalidBoolean(self, value: bool)-> bool:
         """ check if the string is boolean or not """
         if value is None:
             return False
+
         if isinstance(value, bool):
             return True
 
         return False
-    
-    def isAadharCard(self, value):
+
+    def isAadharCard(self, value)-> bool:
         """ check if the string is Aadhar card or not """
-        regex = "^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$"
+        regex = "^[2-9]{1}[0-9]{3}[0-9]{4}[0-9]{4}$"  # need to improve regex for space and hyphen
         value = value.strip().replace(" ", "")
         if self.isLength(value, 12, 12):
-            if isinstance(value, int):  
+            if isinstance(value, int):
                 value = str(value)
-                
+
             if value[0] != '0' or value[0] != '1':
                 if re.match(regex, value):
                     if checksum_aadhar(value):
                         return True
-            
+
         return False
-    
-    def isPostalCode(self, value, locale):
+
+    def isPostalCode(self, value, locale: str)-> bool:
         """ check if the string is postal code or not """
         country_data = all_country[locale]
-        
+
         PostalCode = country_data['PostalCode']
-        
+
         PostalCodeFormat = PostalCode['Format']
         PostalCodeRegex = PostalCode['Regex']
         MinPostalCodeLength = PostalCode['MinLength']
         MaxPostalCodeLength = PostalCode['MaxLength']
-        
+
         if re.match(PostalCodeRegex, value) and re.match(PostalCodeFormat, value) \
             and self.isLength(str(value), MinPostalCodeLength, MaxPostalCodeLength):
                 return True
 
         return False
 
-    def isLicensePlate(self, value, locale):
+    def isLicensePlate(self, value, locale: str) -> bool:
         """ check if the string is license plate or not """
         value = value.upper()
         country_data = all_country[locale]
@@ -83,24 +84,24 @@ class Validator(object):
         Regex = LicensePlate['Regex']
         MinLength = LicensePlate['MinLength']
         MaxLength = LicensePlate['MaxLength']
-        
+
 
         if re.match(Format, value) and re.match(Regex, value) \
             and self.isLength(value, MinLength, MaxLength):
                 return True
 
         return False
-    
-    def isPassportNumber(self, value, locale):  # TODO: research more about passport number
+
+    def isPassportNumber(self, value, locale: str)-> bool:  # TODO: research more about passport number
         """ check if the string is passport number or not """
         country_data = all_country[locale]
-        
+
         PassportNumberRegex = country_data['PassportNumberRegex']
         if re.match(PassportNumberRegex, value):
             return True
         return False
 
-    def isMobilePhone(self, value, locale):
+    def isMobilePhone(self, value, locale: str) -> bool:
         """ check if the string is mobile phone or not """
         country_data = all_country[locale]
 
@@ -110,8 +111,8 @@ class Validator(object):
             return True
 
         return False
-    
-    def isDriverLicense(self, value, locale):
+
+    def isDriverLicense(self, value, locale: str) -> bool:
         """ check if the string is driver license or not """
         country_data = all_country[locale]
 
@@ -121,24 +122,24 @@ class Validator(object):
             return True
 
         return False
-    
-    def isDiscordUsername(self, value):
+
+    def isDiscordUsername(self, value: str) -> bool:
         regex = "^.{3,32}#[0-9]{4}$"
         if re.match(regex, value):
             return True
         return False
-    
-    def isCreditCard(self, value):  # checksum not implemented
+
+    def isCreditCard(self, value: str) -> bool:  # checksum not implemented
         regex = regexs['credit_card_regex']
         if re.match(regex, value):
             return True
         return False
 
-    def equals(self, value1, value2, ignoreCase=False)-> bool:
+    def equals(self, value1: str, value2: str, ignoreCase: bool=False)-> bool:
         """ Check if the two string are equal or not """
         if not self.__isvalidString(value1) or not self.__isvalidString(value2):
             return False
-        
+
         if ignoreCase:
             value1 = value1.lower()
             value2 = value2.lower()
@@ -148,14 +149,14 @@ class Validator(object):
 
         return False
 
-    def isLength(self, value, min=0, max=None):
+    def isLength(self, value: str, min: int=0, max: int=None) -> bool:
         """ check if the string length is between min and max """
         if min <= len(value) <= max:
             return True
 
         return False
 
-    def isEmpty(self, value):
+    def isEmpty(self, value: str) -> bool:
         """ check if the string is empty or not """
         value = value.strip()
         if value == "":
@@ -163,25 +164,25 @@ class Validator(object):
 
         return False
 
-    def isAlphanumeric(self, value):
+    def isAlphanumeric(self, value: str) -> bool:
         if value.isalnum():
             return True
 
         return False
 
-    def isAlpha(self, value):
+    def isAlpha(self, value: str) -> bool:
         if value.isalpha():
             return True
 
         return False
 
-    def isAscii(self, value):
+    def isAscii(self, value) -> bool:
         if value.isascii():
             return True
 
         return False
 
-    def contains(self, value, substring, ignoreCase=False):
+    def contains(self, value: str, substring: str, ignoreCase: bool=False) -> bool:
         if ignoreCase:
             value = value.lower()
             substring = substring.lower()
@@ -191,7 +192,7 @@ class Validator(object):
 
         return False
 
-    def isAfter(self, date1, date2=datetime.now()):
+    def isAfter(self, date1, date2=datetime.now()) -> bool:
         """ check if date1 is after date2 default date2 is current date
 
         Args:
@@ -210,7 +211,7 @@ class Validator(object):
 
         return False
 
-    def isBefore(self, date1, date2=datetime.now()):
+    def isBefore(self, date1, date2=datetime.now()) -> bool:
         """
         check if date1 is before date2 default date2 is current date
 
@@ -228,7 +229,7 @@ class Validator(object):
 
         return False
 
-    def isDate(self, value):
+    def isDate(self, value) -> bool:
         """ check if the string is date or not """
         if not self.__isvalidString(value):
             return False
@@ -239,8 +240,8 @@ class Validator(object):
         except Exception:
             return False
         return False
-    
-    def isLeapYear(self, year):
+
+    def isLeapYear(self, year) -> bool:
         """ check if the year is leap year or not """
         if year % 4 == 0:
             if year % 100 == 0:
@@ -254,7 +255,7 @@ class Validator(object):
             return False
 
 
-    def isEmail(self, value, checkDNS=False):
+    def isEmail(self, value: str, checkDNS: bool=False) -> bool:
         """ check if the string is email or not """
         if not self.__isvalidString(value):
             return False
@@ -264,17 +265,17 @@ class Validator(object):
 
         return False
 
-    def isDecimal(self, value):
+    def isDecimal(self, value: float) -> bool:
         """ check if the string is decimal or not """
         if not self.__isvalidNumber(value):
             return False
-    
+
         if value.isdecimal():
             return True
 
         return False
 
-    def isDivisibleBy(self, number, divisor):
+    def isDivisibleBy(self, number: int, divisor: int) -> bool:
         """ check if the number is divisible by divisor or not """
         if self.__isvalidNumber(number) and self.__isvalidNumber(divisor) and divisor != 0:
             if number % divisor == 0:
@@ -282,41 +283,41 @@ class Validator(object):
 
         return False
 
-    def isEAN(self, value):
+    def isEAN(self, value) -> bool:
         """ check if the string is EAN or not """
         pass
 
-    def isHash(self, value):
+    def isHash(self, value) -> bool:
         """ check if the string is hash or not """
         pass
 
-    def isIMEI(self, value):
+    def isIMEI(self, value) -> bool:
         """ check if the string is IMEI or not """
         if len(value) == 15 or len(value) == 17:
             return True
         return False
 
-    def isIPV4(self, value):
+    def isIPV4(self, value: str) -> bool:
         """ check if the string is IP or not """
         regex = regexs['ipv4_regex']
         if re.search(regex, value):
             return True
         return False
 
-    def isIPV6(self, value):
+    def isIPV6(self, value: str) -> bool:
         regex = regexs['ipv6_regex']
         if re.match(regex, value):
             return True
         return False
 
-    def isSSN(self, value):
+    def isSSN(self, value) -> bool:
         """ check if the string is SSN or not """
         regex = '^(?!0{3})(?!6{3})[0-8]\d{2}-(?!0{2})\d{2}-(?!0{4})\d{4}$'
         if re.match(regex, value):
             return True
         return False
-    
-    def isJSON(self, value):
+
+    def isJSON(self, value) -> bool:
         """ check if the string is JSON or not """
         try:
             json.loads(value)
@@ -324,7 +325,7 @@ class Validator(object):
         except ValueError:
             return False
 
-    def isJWT(self, value):
+    def isJWT(self, value) -> bool:
         """ check if the string is JWT or not """
         regex = "^[A-Za-z0-9_-]{2,}(?:\.[A-Za-z0-9_-]{2,}){2}$"
         if re.match(regex, value):
@@ -332,26 +333,25 @@ class Validator(object):
 
         return False
 
-    def isLatLong(self, value):
+    def isLatLong(self, value: str) -> bool:
         """ check if the string is lat long or not """
         regex = "^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$"
         if re.match(regex, value):
             return True
         return False
 
-    def isMACAddress(self, value):
+    def isMACAddress(self, value: str) -> bool:
         """ check if the string is MAC address or not """
         regex = regexs['mac_address_regex']
         if self.__isvalidString(value) and re.search(regex, value, re.IGNORECASE):
             return True
         return False
 
-    def isMD5(self, value):
+    def isMD5(self, value) -> bool:
         """ check if the string is MD5 or not """
         pass
 
-   
-    def isPort(self, value):
+    def isPort(self, value: int) -> bool:
         """ check if the string is port or not
         A port number is a 16-bit unsigned integer,
         so it has a minimum value of 0 and a maximum value of 65535.
@@ -360,11 +360,11 @@ class Validator(object):
             return True
         return False
 
-    def isSlug(self, value):
+    def isSlug(self, value: str) -> bool:
         """ check if the string is slug or not """
         pass
 
-    def isStrongPassword(self, value):
+    def isStrongPassword(self, value: str) -> bool:
         """ check if the string is strong password or not
 
         requirements:
@@ -391,52 +391,50 @@ class Validator(object):
 
         return True
 
-    def isUUID(self, value):
+    def isUUID(self, value: str) -> bool:
         """ check if the string is UUID or not """
         pass
 
-    # Sanitizers functions
     def toDate(self, value):  # need to improve this function
         """ convert string to date """
         format = '%Y-%m-%d'
         if self.isDate(value):
             date = datetime.strptime(value, format)
             return date
-        
+
         else:
             return None
-        
-    
+
     def toInt(self, value):
         """ convert string to int """
         if isinstance(value, int):
             return value
-        
+
         elif isinstance(value, float):
             return int(value)
-        
+
         try:
             value = int(float(value))
             return value
-        
+
         except ValueError:
             value = 0
-        
+
         return value
 
     def trim(self, value):
         """ trim string """
         if self.__isvalidString(value):
             return value.strip()
-        
+
     def ltrim(self, value):
         if self.__isvalidString(value):
             return value.lstrip()
-        
+
     def rtrim(self, value):
         if self.__isvalidString(value):
             return value.rstrip()
-        
+
     def toUpperCase(self, value):
         """ convert string to upper case """
         if self.__isvalidString(value):
@@ -451,17 +449,17 @@ class Validator(object):
         """ remove spaces from string """
         if self.__isvalidString(value):
             return value.replace(" ", "")
-        
+
     def removeSymbols(self, value):
         """ remove symbols from string """
         if self.__isvalidString(value):
             return re.sub(r'[^\w\s]', '', value)
-    
+
     def levenshtein_distance(self, value1, value2):
         """ calculate distance between two strings """
         distance = levenshtein_distance(value1, value2)
         return distance
-    
+
     def edit_distance(self, value1, value2):
         """ calculate distance between two strings """
         if len(value1) > len(value2):
@@ -488,7 +486,7 @@ class Validator(object):
         """ remove non word characters from string """
         if self.__isvalidString(value):
             return re.sub(r'[^\w]', '', value)
-        
+
     def removeNonNumeric(self, value):
         """ remove non numeric characters from string """
         if self.__isvalidString(value):
